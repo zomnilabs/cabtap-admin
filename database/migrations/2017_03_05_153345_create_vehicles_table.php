74e0a6f90d5c31edc8b04aa636989c44378de8e9
Alleo Indong <freedom_war550@live.com.ph>
2017-03-05 23:50:39 +0800

    added vehicle model

---
+++ database/migrations/2017_03_05_153345_create_vehicles_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVehiclesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('make');
+            $table->string('model');
+            $table->string('year');
+            $table->string('plate_number');
+            $table->enum('status', ['inactive', 'active'])->default('inactive');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicles');
+    }
+}
