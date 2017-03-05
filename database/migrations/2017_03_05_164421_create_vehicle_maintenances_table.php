220b8642f2b813c7befaf0275441e61082a70854
Alleo Indong <freedom_war550@live.com.ph>
2017-03-06 00:47:09 +0800

    added vehicle maintenance

---
+++ database/migrations/2017_03_05_164421_create_vehicle_maintenances_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVehicleMaintenancesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vehicle_maintenances', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('vehicle_id')->unsigned();
+            $table->string('name');
+            $table->double('price');
+            $table->date('scheduled_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vehicle_maintenances');
+    }
+}
