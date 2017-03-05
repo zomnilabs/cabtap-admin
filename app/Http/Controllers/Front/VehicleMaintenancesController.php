4481e289dbec6a00c362cc6bac0b334f672e0d84
Alleo Indong <freedom_war550@live.com.ph>
2017-03-06 01:17:34 +0800

    added some controller

---
+++ app/Http/Controllers/Front/VehicleMaintenancesController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Http\Controllers\Controller;
+use App\VehicleMaintenance;
+use Illuminate\Http\Request;
+
+class VehicleMaintenancesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\VehicleMaintenance  $vehicleMaintenance
+     * @return \Illuminate\Http\Response
+     */
+    public function show(VehicleMaintenance $vehicleMaintenance)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\VehicleMaintenance  $vehicleMaintenance
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(VehicleMaintenance $vehicleMaintenance)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\VehicleMaintenance  $vehicleMaintenance
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, VehicleMaintenance $vehicleMaintenance)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\VehicleMaintenance  $vehicleMaintenance
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(VehicleMaintenance $vehicleMaintenance)
+    {
+        //
+    }
+}
