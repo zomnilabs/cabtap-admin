ecac06377d203e960038a4616d1d9cd662abd800
Alleo Indong <freedom_war550@live.com.ph>
2017-03-06 14:10:55 +0800

    home and app blade template

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -20,6 +20,12 @@ require('bootstrap-sass');
 window.Vue = require('vue');
 
 /**
+ * ChartJS
+ */
+
+window.Chart = require('chart.js');
+
+/**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,108 @@
 @extends('layouts.app')
 
+@section('scripts')
+    <script>
+        (function() {
+            let ctx = document.getElementById("myChart");
+
+            let data = {
+                labels: ["January", "February", "March", "April", "May", "June", "July"],
+                datasets: [
+                    {
+                        label: "Requested Trips",
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: "rgba(208,167,88,0.4)",
+                        borderColor: "rgba(208,167,88,1)",
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: "rgba(208,167,88,1)",
+                        pointBackgroundColor: "#fff",
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: "rgba(208,167,88,1)",
+                        pointHoverBorderColor: "rgba(220,220,220,1)",
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                        data: [65, 59, 80, 81, 56, 55, 40],
+                        spanGaps: false,
+                    },
+                    {
+                        label: "Accepted Trips",
+                        fill: false,
+                        lineTension: 0.1,
+                        backgroundColor: "rgba(75,192,192,0.4)",
+                        borderColor: "rgba(75,192,192,1)",
+                        borderCapStyle: 'butt',
+                        borderDash: [],
+                        borderDashOffset: 0.0,
+                        borderJoinStyle: 'miter',
+                        pointBorderColor: "rgba(75,192,192,1)",
+                        pointBackgroundColor: "#fff",
+                        pointBorderWidth: 1,
+                        pointHoverRadius: 5,
+                        pointHoverBackgroundColor: "rgba(75,192,192,1)",
+                        pointHoverBorderColor: "rgba(220,220,220,1)",
+                        pointHoverBorderWidth: 2,
+                        pointRadius: 1,
+                        pointHitRadius: 10,
+                        data: [60, 50, 70, 80, 50, 50, 39],
+                        spanGaps: false,
+                    }
+                ]
+            };
+
+            let myChart = new Chart(ctx, {
+                type: 'line',
+                data: data,
+                option: {
+                    maintainAspectRatio: false,
+                    responsive: true
+                }
+            });
+        }())
+    </script>
+@endsection
+
 @section('content')
 <div class="container-fluid">
     <div class="row">
-        <div class="col-md-12">
+        <div class="col-md-4">
             <div class="panel panel-default">
-                <div class="panel-heading">Dashboard</div>
-
                 <div class="panel-body">
-                    You are logged in!
+                    <h3>10</h3>
+                    <h5>Active Vehicles</h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h3>10</h3>
+                    <h5>Vehicles Under Maintenance</h5>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-md-4">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <h3>100</h3>
+                    <h5>Completed Trips</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6">
+            <div class="panel panel-default">
+                <div class="panel-body">
+                    <canvas id="myChart" width="400" height="200"></canvas>
                 </div>
             </div>
         </div>
