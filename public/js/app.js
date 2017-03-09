6fae581e81d3d87c3bdd0ee7b214db91e6f9e1f0
Alleo Indong <freedom_war550@live.com.ph>
2017-03-09 10:55:59 +0800

    updated appjs

--- public/js/app.js
+++ public/js/app.js
@@ -59028,7 +59028,7 @@ var Component = __webpack_require__(193)(
   /* cssModules */
   null
 )
-Component.options.__file = "/Users/nkmcheng/Public/projects/zomnilabs/cabtap-admin/resources/assets/js/components/Example.vue"
+Component.options.__file = "/Users/alleoindong/Public/projects/aindong/cabtap/cabtap-admin/resources/assets/js/components/Example.vue"
 if (Component.esModule && Object.keys(Component.esModule).some(function (key) {return key !== "default" && key !== "__esModule"})) {console.error("named exports are not supported in *.vue files.")}
 if (Component.options.functional) {console.error("[vue-loader] Example.vue: functional components are not supported with templates, they should use render functions.")}
 
