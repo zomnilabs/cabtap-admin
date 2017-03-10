7742ba9d24f7128030fd9e4198a4b1b02fe6ff38
Alleo Indong <freedom_war550@live.com.ph>
2017-03-10 21:20:05 +0800

    added announcements migration and model

---
+++ database/migrations/2017_03_10_131721_create_announcements_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnnouncementsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('announcements', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('target_user_id')->nullable();
+            $table->integer('target_user_group_id')->nullable();
+            $table->enum('type', ['single', 'group'])->default('single');
+            $table->string('title');
+            $table->text('message');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('announcements');
+    }
+}
