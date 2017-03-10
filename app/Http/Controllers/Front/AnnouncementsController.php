ec781707c9bcf79e5e943b9cb7563b6bfe6294b9
Alleo Indong <freedom_war550@live.com.ph>
2017-03-11 01:43:41 +0800

    added announcement

---
+++ app/Http/Controllers/Front/AnnouncementsController.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace App\Http\Controllers\Front;
+
+use App\Announcement;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class AnnouncementsController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $announcements = Announcement::all();
+
+        return view('announcements.index', compact('announcements'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Announcement  $announcement
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Announcement $announcement)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Announcement  $announcement
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Announcement $announcement)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Announcement  $announcement
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Announcement $announcement)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Announcement  $announcement
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Announcement $announcement)
+    {
+        //
+    }
+}
---
+++ resources/views/announcements/index.blade.php
@@ -0,0 +1,67 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Announcements</div>
+
+                    <div class="panel-body">
+                        <a href="/announcements/create" class="btn btn-primary">
+                            <i class="glyphicon glyphicon-plus"></i> Create New Announcement
+                        </a>
+                        <br><br>
+                        <table class="table table-bordered">
+                            <thead>
+                            <tr>
+                                <th>Id</th>
+                                <th>Vehicle</th>
+                                <th>Full Name</th>
+                                <th>Created Date</th>
+                                <th>Actions</th>
+                            </tr>
+                            </thead>
+
+                            <tbody>
+                            @foreach($drivers as $driver)
+                                <tr>
+                                    <td>{{ $driver->id }}</td>
+                                    <td>{{ $driver->vehicle->make }} {{ $driver->vehicle->model }} {{ $driver->vehicle->year }} : {{ $driver->vehicle->plate_number }}</td>
+                                    <td>{{ $driver->user->profile->getFullNameAttribute() }}</td>
+                                    <td>{{ $driver->created_at->toFormattedDateString() }}</td>
+                                    <td>
+                                        <a class="btn btn-primary" href="/drivers/{{ $driver->id }}/edit">
+                                            <i class="glyphicon glyphicon-edit"></i>
+                                        </a>
+
+                                        <button class="btn btn-danger"
+                                                onclick="event.preventDefault();
+                                                        document.getElementById('delete-driver-form-{{ $driver->id }}').submit();">
+                                            <i class="glyphicon glyphicon-trash"></i>
+                                        </button>
+
+                                        <form id="delete-driver-form-{{ $driver->id }}" action="/drivers/{{ $driver->id }}" method="POST" style="display: none;">
+                                            {{ method_field('DELETE') }}
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </td>
+                                </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <iframe src="/drivers/preview" name="frame" style="width: 0; height: 0"></iframe>
+@endsection
+
+@section('scripts')
+    <script>
+        $(function() {
+            $('.table').DataTable();
+        });
+    </script>
+@endsection
