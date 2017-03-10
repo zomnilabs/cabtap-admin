aed6c124235ec11701fbfc740720651c65d03c0c
Alleo Indong <freedom_war550@live.com.ph>
2017-03-10 22:30:47 +0800

    updated fcm

--- config/fcm.php
+++ config/fcm.php
@@ -5,8 +5,8 @@ return [
 	'log_enabled' => true,
 
 	'http' => [
-		'server_key'       => env('FCM_SERVER_KEY', 'Your FCM server key'),
-		'sender_id'        => env('FCM_SENDER_ID', 'Your sender id'),
+		'server_key'       => env('FCM_SERVER_KEY', 'AAAAz5jbgvI:APA91bHhWv5Ex-k7rgy0XwhYi1mUGP5VWuHCR10oGI7o7skWWE_agrMTmunp876j9tCGM4V8DjvqFV7dKpO_EDegSGeLQu9W4Qe276VpG7S8MYipfpLXYNvwcpyJMMa1rxM4vTCY3nUHeG0Dwp1Cn_d6sQb1zDNyVA'),
+		'sender_id'        => env('FCM_SENDER_ID', '891622753010'),
 		'server_send_url'  => 'https://fcm.googleapis.com/fcm/send',
 		'server_group_url' => 'https://android.googleapis.com/gcm/notification',
 		'timeout'          => 30.0, // in second
